94a4f2a270431c9134737b66e087e2c78fba73d7
kiten46087 <592692725@qq.com>
2020-02-21 11:34:42 -0800

    Add Solution to CC189 both recursive approach and memorization approach

---
+++ RecursionAndDp/BooleanEvaluation.java
@@ -0,0 +1,81 @@
+package RecursionAndDp;
+
+import java.util.HashMap;
+
+public class BooleanEvaluation {
+    // Recursive ways to do this problem we can also do this in the memorization solution(DP).
+    public int countEval(String s, boolean result) {
+        // Base Cases
+        if (s.length() == 0) return 0;
+        if (s.length() == 1) return stringToBool(s) == result ? 1 : 0;
+
+        int ways = 0;
+        for (int i = 1; i < s.length(); i += 2) {
+            char c = s.charAt(i);
+            String left = s.substring(0, i);
+            String right = s.substring(i + 1, s.length());
+
+            // Evaluate each side for each result
+            int leftTrue = countEval(left, true);
+            int leftFalse = countEval(left, false);
+            int rightTrue = countEval(right, true);
+            int rightFalse = countEval(right, false);
+            int total = (leftTrue + leftFalse) * (rightTrue + rightFalse);
+
+            int totalTrue = 0;
+            if (c == '^') {
+                totalTrue = leftTrue * rightFalse + leftFalse * rightTrue;
+            } else if (c == '&') {
+                totalTrue = leftTrue * rightTrue;
+            } else if (c == '|') {
+                totalTrue = leftTrue * rightTrue + leftFalse * rightTrue + leftTrue * rightFalse;
+            }
+
+            int subWays = result ? totalTrue : total - totalTrue;
+            ways += subWays;
+        }
+
+        return ways;
+    }
+
+    public boolean stringToBool(String s) {
+        return s.equals("1") ? true : false;
+    }
+
+    // This is the dynamic programming using memorization approach.
+    public int countEval(String s, boolean result, HashMap<String, Integer> memo) {
+        // Base Cases
+        if (s.length() == 0) return 0;
+        if (s.length() == 1) return stringToBool(s) == result ? 1 : 0;
+        if (memo.containsKey(result + s)) return memo.get(result + s);
+
+        int ways = 0;
+        for (int i = 1; i < s.length(); i += 2) {
+            char c = s.charAt(i);
+            String left = s.substring(0, i);
+            String right = s.substring(i + 1, s.length());
+
+            // Evaluate each side for each result
+            int leftTrue = countEval(left, true, memo);
+            int leftFalse = countEval(left, false, memo);
+            int rightTrue = countEval(right, true, memo);
+            int rightFalse = countEval(right, false, memo);
+            int total = (leftTrue + leftFalse) * (rightTrue + rightFalse);
+
+            int totalTrue = 0;
+            if (c == '^') {
+                totalTrue = leftTrue * rightFalse + leftFalse * rightTrue;
+            } else if (c == '&') {
+                totalTrue = leftTrue * rightTrue;
+            } else if (c == '|') {
+                totalTrue = leftTrue * rightTrue + leftFalse * rightTrue + leftTrue * rightFalse;
+            }
+
+            int subWays = result ? totalTrue : total - totalTrue;
+            ways += subWays;
+        }
+
+        memo.put(result + s, ways);
+        return ways;
+    }
+}
